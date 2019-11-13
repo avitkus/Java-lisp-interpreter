6a3e9459dce95ad46524a0aade191497c7b1974d
Andrew Vitkus <avitkus7@gmail.com>
2019-11-13 16:09:36 -0500

    ArgumentEvaluator.split is now static

--- src/main/lisp/evaluator/parallel/args/ArgumentEvaluator.java
+++ src/main/lisp/evaluator/parallel/args/ArgumentEvaluator.java
@@ -1,13 +1,22 @@
 package main.lisp.evaluator.parallel.args;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import main.lisp.evaluator.Environment;
+import main.lisp.parser.terms.NilAtom;
 import main.lisp.parser.terms.SExpression;
 
 public interface ArgumentEvaluator {
 
-	public List<SExpression> split(SExpression expr);
+	public static List<SExpression> split(SExpression expr) {
+		List<SExpression> parts = new ArrayList<>();
+		while (!(expr instanceof NilAtom)) {
+			parts.add(expr.getHead());
+			expr = expr.getTail();
+		}
+		return parts;
+	}
 
 	public List<SExpression> evaluate(List<SExpression> expressions, Environment env);
 }
--- src/main/lisp/evaluator/parallel/args/NullArgumentEvaluator.java
+++ src/main/lisp/evaluator/parallel/args/NullArgumentEvaluator.java
@@ -8,11 +8,6 @@ import main.lisp.parser.terms.SExpression;
 public final class NullArgumentEvaluator implements ArgumentEvaluator {
 
 	@Override
-	public List<SExpression> split(SExpression expr) {
-		throw new UnsupportedOperationException("To use this you must register a new class with the ArgumentEvaluatorSingleton");
-	}
-
-	@Override
 	public List<SExpression> evaluate(List<SExpression> expressions, Environment env) {
 		throw new UnsupportedOperationException("To use this you must register a new class with the ArgumentEvaluatorSingleton");
 	}

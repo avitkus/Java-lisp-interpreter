9abd6728a6d99c7589b1e12d6492fa30a69f2129
dewan <dewan@cs.unc.edu>
2021-11-26 15:20:01 -0500

    Made ExpressionEvalauator extend Evaluator making them equal

--- src/main/lisp/evaluator/ExpressionEvaluator.java
+++ src/main/lisp/evaluator/ExpressionEvaluator.java
@@ -2,7 +2,7 @@ package main.lisp.evaluator;
 
 import main.lisp.parser.terms.SExpression;
 
-public interface ExpressionEvaluator {
-	public SExpression eval(SExpression expr, Environment env);
-	public SExpression lazyEval(SExpression expr, Environment env);
+public interface ExpressionEvaluator extends Evaluator{
+//	public SExpression eval(SExpression expr, Environment env);
+//	public SExpression lazyEval(SExpression expr, Environment env);
 }
